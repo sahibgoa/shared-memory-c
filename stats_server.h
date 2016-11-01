af45f602a2562b746636e3b22c5cd61e6ccf0322
Haylee Jane Monteiro <haylee.jane3@gmail.com>
2016-11-01 15:38:58 -0500

    removed unnecessary header files

--- stats_server.h
+++
@@ -1,9 +0,0 @@
-
-/* Checks whether the given string is a number or not */
-int is_number(char *num);
-
-/* Calculates number of characters required to represent an integer */
-int num_chars_in_int(int integer);
-
-/* Program starts here */
-int main(int argc, char* argv[]);
